f9c47fd858f6dc7e78d30bda6ea02cdf229cc1f7
Ritika Gutgutia <gutgutiaritika@gmail.com>
2024-11-24 20:30:33 +0530

    enabled login persistance until user logs out or token gets expired, enabled auto redirects to login page when user not logged in, enabled link to profile on profile link click, enabled link to home page on logo click, enabled logout feature

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useDispatch } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      console.log(res, "I am res!!");
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,35 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import axios from "axios";
+import { removeUser } from "../utils/userSlice";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
-  console.log(user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        BASE_URL + "/logout",
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (err) {
+      //handle error logic
+    }
+  };
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🚀DevConnect</a>
+        <Link to="/" className="btn btn-ghost text-xl">
+          🚀DevConnect
+        </Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
@@ -27,16 +49,16 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
               </li>
               <li>
-                <a>Logout</a>
+                <a onClick={() => handleLogout()}>Logout</a>
               </li>
             </ul>
           </div>
